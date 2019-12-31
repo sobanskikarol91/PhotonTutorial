c166d34a87c1221cc0603edd3aefa29b26db0874
Karol Sobański <sobanskikarol91@gmail.com>
2019-12-31 13:23:43 +0100

    Update Launcher

--- PhotonTutorial/Assets/Scripts/Launcher.cs
+++ PhotonTutorial/Assets/Scripts/Launcher.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
-using System.Linq;
-using Photon.Pun;
 
-public class Launcher : MonoBehaviour 
+public class Launcher : MonoBehaviourPunCallbacks
 {
-    string gameVersion = "1";
+    private string gameVersion = "1";
+    [SerializeField] byte maxPlayersPErRoom = 4;
+
 
     private void Awake()
     {
@@ -28,5 +27,26 @@ public class Launcher : MonoBehaviour
             PhotonNetwork.GameVersion = gameVersion;
             PhotonNetwork.ConnectUsingSettings();
         }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = maxPlayersPErRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
     }
 }

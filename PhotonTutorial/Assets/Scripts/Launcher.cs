c04df3eb87b383843a2fa699bcb7a976ee1a5888
Karol Sobański <sobanskikarol91@gmail.com>
2019-12-31 13:06:29 +0100

    Add: Photon

---
+++ PhotonTutorial/Assets/Scripts/Launcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviour 
+{
+    string gameVersion = "1";
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        Connect();
+    }
+
+    private void Connect()
+    {
+        if (PhotonNetwork.IsConnected)
+            PhotonNetwork.JoinRandomRoom();
+        else
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+}

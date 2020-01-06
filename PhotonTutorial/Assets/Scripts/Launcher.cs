16dc0d06b62e0da684568500364aa5f38cf18635
Karol Sobański <sobanskikarol91@gmail.com>
2020-01-06 13:36:54 +0100

    Update: Lanucher scene

--- PhotonTutorial/Assets/Scripts/Launcher.cs
+++ PhotonTutorial/Assets/Scripts/Launcher.cs
@@ -7,14 +7,29 @@ public class Launcher : MonoBehaviourPunCallbacks
     private string gameVersion = "1";
     [SerializeField] byte maxPlayersPErRoom = 4;
 
+    [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+    [SerializeField] private GameObject controlPanel;
+    [Tooltip("The UI Label to inform the user that the connection is in progress")]
+    [SerializeField] private GameObject progressLabel;
+
 
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
+        HideProgressBar();
+    }
+
+    private void HideProgressBar()
+    {
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
     }
 
     public void Connect()
     {
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
+
         if (PhotonNetwork.IsConnected)
             PhotonNetwork.JoinRandomRoom();
         else
@@ -32,6 +47,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        HideProgressBar();
     }
 
     public override void OnJoinRandomFailed(short returnCode, string message)
---
+++ PhotonTutorial/Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class PlayerNameInputField : MonoBehaviour 
+{
+    const string playerNamePrefKey = "PlayerName";
+
+    private void Start()
+    {
+        string defaultName = string.Empty;
+        InputField inputField = GetComponent<InputField>();
+
+        if(inputField != null)
+        {
+            defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+            inputField.text = defaultName;
+        }
+
+        PhotonNetwork.NickName = defaultName;
+    }
+
+    public void SetName(string value)
+    {
+        // #Important
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Player Name is null or empty");
+            return;
+        }
+        PhotonNetwork.NickName = value;
+
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}

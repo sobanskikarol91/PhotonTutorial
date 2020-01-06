0b1792ff753c3b8bb5d5ec13805980268b4f5e1f
Karol Sobański <sobanskikarol91@gmail.com>
2020-01-06 21:39:21 +0100

    Add Player

---
+++ PhotonTutorial/Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerAnimatorManager : MonoBehaviour 
+{
+    private Animator animator;
+
+    [SerializeField]
+    private float directionDampTime = 0.25f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (!animator)
+        {
+            Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+        }
+    }
+
+    private void Update()
+    {
+        if (!animator)
+        {
+            return;
+        }
+
+        // deal with Jumping
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        // only allow jumping if we are running.
+        if (stateInfo.IsName("Base Layer.Run"))
+        {
+            // When using trigger parameter
+            if (Input.GetButtonDown("Fire2"))
+            {
+                animator.SetTrigger("Jump");
+            }
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if (v < 0)
+        {
+            v = 0;
+        }
+
+        animator.SetFloat("Speed", h * h + v * v);
+        animator.SetFloat("Direction", h, directionDampTime, Time.deltaTime);
+    }
+}
